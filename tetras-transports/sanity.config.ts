6d1c454022d94e9a96f1ed87f244431f1f40bb6c
B0TAstro <148534949+B0TAstro@users.noreply.github.com>
2025-03-07 09:21:28 +0100

    first commit

---
+++ tetras-transports/sanity.config.ts
@@ -0,0 +1,20 @@
+// sanity.config.ts
+
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemaTypes'
+
+export default defineConfig({
+  name: 'tetras-transport',
+  title: 'Tetras Transport',
+
+  projectId: 'yukqhttc',
+  dataset: 'production',
+  basePath: "/studio",
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
