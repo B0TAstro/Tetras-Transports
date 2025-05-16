25ff6f0feab5f4d54c1f3f8a01bd15d67a6cb41d
Tom Boullay <148534949+B0TAstro@users.noreply.github.com>
2025-05-16 19:37:32 +0200

    bon c'est chiant à regler le header donc on va faire comme ça

--- app/politique-confidentialite/page.tsx
+++ app/politique-confidentialite/page.tsx
@@ -3,6 +3,30 @@
 export default function PolitiqueConfidentialite() {
   return (
     <main className="max-w-3xl h-screen mx-auto p-8 space-y-6 mt-35">
+
+      <div className="mb-8">
+        <a
+          href="/"
+          className="inline-flex items-center text-lg font-medium px-3 py-2 rounded-sm duration-250 border border-dark hover:text-blue-700 hover:border-blue-700"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          Retour à l'accueil
+        </a>
+      </div>
+      
       <h1 className="text-3xl font-bold">Politique de confidentialité</h1>
       <p>
         Nous nous engageons à protéger votre vie privée et vos données
